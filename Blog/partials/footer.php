83e3215c3ace04461d44ef0c9b2a5e56d8e92c8f
rajsinghjio2017-dotcom <rajsinghjio2017@gmail.com>
2026-01-13 13:55:51 +0530

    Create footer.php

---
+++ Blog/partials/footer.php
@@ -0,0 +1,58 @@
+<footer>
+    <div class="footer__socials">
+        <!-- iconscout.com/unicons/explore/line -->
+        <a href="" target="_blank"><img src="<?= ROOT_URL ?>./icons/github.png"></a>
+        <a href="" target="_blank"><img src="<?= ROOT_URL ?>./icons/instagram.png"></a>
+        <a href="" target="_blank"><img src="<?= ROOT_URL ?>./icons/whatsapp.png"></a>
+        <a href="" target="_blank"><img src="<?= ROOT_URL ?>./icons/linkedin-logo.png"></a>
+        <a href="" target="_blank"><img src="<?= ROOT_URL ?>./icons/facebook.png"></a>
+    </div>
+    <div class="container footer__container">
+        <article>
+            <h4>Support</h4>
+            <ul>
+                <li><a href="">Github</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Youtube</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="">Github</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Youtube</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="">Github</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Youtube</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+            </ul>
+        </article>
+        <article>
+            <h4>Blog</h4>
+            <ul>
+                <li><a href="">Github</a></li>
+                <li><a href="">Twitter</a></li>
+                <li><a href="">Youtube</a></li>
+                <li><a href="">Facebook</a></li>
+                <li><a href="">Instagram</a></li>
+            </ul>
+        </article>
+    </div>
+    <div class="footer__copyright">
+        <small>Copyright &copy; 2023 MANU </small>
+    </div>
+</footer>
+<script src="<?= ROOT_URL ?>js/main.js"></script>
+</body>
+</html>
